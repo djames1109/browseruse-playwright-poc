2f24fa7e1adfeb82640704c49ec5c564c4ca1ce8
DJames <davejames1109@gmail.com>
2025-04-19 23:54:20 +0800

    Add playwright test script to login to shettyacademy page

--- playwright/test_playwrightBasic.py
+++ playwright/test_playwrightBasic.py
@@ -1,3 +1,5 @@
+import time
+
 from playwright.sync_api import Playwright, Page
 
 
@@ -12,3 +14,20 @@ def test_playwright_basic(playwright: Playwright):
 # chromium headless mode, 1 single context
 def test_playwright_shortcut(page: Page):
     page.goto("https://www.google.com")
+
+
+def test_core_locators(playwright):
+    page = get_headed_browser(playwright).new_page()
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("learning")
+    page.get_by_role("combobox").select_option("teach")
+    page.get_by_role("checkbox").check()
+    page.get_by_role("button").click()
+    time.sleep(5)
+
+
+def get_headed_browser(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    return context
