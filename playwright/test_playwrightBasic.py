ea502c4cc99671fb65ccf783aebd3d95fead0ae0
DJames <davejames1109@gmail.com>
2025-04-19 22:51:40 +0800

    Add shortcut way to use browser page

--- playwright/test_playwrightBasic.py
+++ playwright/test_playwrightBasic.py
@@ -1,8 +1,14 @@
+from playwright.sync_api import Playwright, Page
 
 
 # playwright param below is a fixture from pytest-playwright package.
-def test_playwright_basic(playwright):
+def test_playwright_basic(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context() # like opening in incognito, once you exit, will reset session, cookies, etc
+    context = browser.new_context()  # like opening in incognito, once you exit, will reset session, cookies, etc
     page = context.new_page()
+    page.goto("https://www.google.com")
+
+
+# chromium headless mode, 1 single context
+def test_playwright_shortcut(page: Page):
     page.goto("https://www.google.com")
