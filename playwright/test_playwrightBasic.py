06b0aa54f016f56d2ce351f377aecbfe53a06387
DJames <davejames1109@gmail.com>
2025-04-20 01:49:00 +0800

    Add test addToCart

--- playwright/test_playwrightBasic.py
+++ playwright/test_playwrightBasic.py
@@ -40,6 +40,28 @@ def test_loginPage_invalidPassword(page: Page):
     expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
 
 
+def test_addToCart2_success(playwright: Playwright):
+    # login
+    page = get_headed_browser(playwright).new_page()
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_label("Username:").fill(
+        "rahulshettyacademy")  # only works if the label is associated with the text box using 'for'.
+    page.get_by_label("Password:").fill("learning")
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()  # CSS locator. this one selects the checkbox and mark it as checked
+    page.get_by_role("button", name="Sign In").click()
+
+    #     Add to Cart
+    iphoneX = page.locator("app-card").filter(has_text="iphone X")
+    iphoneX.get_by_role("button").click()
+
+    nokiaEdge = page.locator("app-card").filter(has_text="Nokia Edge")
+    nokiaEdge.get_by_role("button").click()
+
+    page.get_by_text("Checkout").click()  # Checkout button
+
+    expect(page.locator(".media")).to_have_count(2)
+
 # ==================== Helper functions ===========================
 
 def get_headed_browser(playwright):
