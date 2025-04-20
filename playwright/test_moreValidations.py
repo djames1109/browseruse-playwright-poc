bbf3b6cba7467e5f3655a095cbd423c18f1543df
DJames <davejames1109@gmail.com>
2025-04-20 17:42:33 +0800

    Interact with pop up box

--- playwright/test_moreValidations.py
+++ playwright/test_moreValidations.py
@@ -1,10 +1,23 @@
-from playwright.sync_api import Playwright, Page, expect
+import time
+
+from playwright.sync_api import Page, expect, Playwright
 
 
-def test_hideShowUIChecks(page:Page):
+def test_hideShowUIChecks(page: Page):
     page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_visible()
     page.get_by_role("button", name="Hide").click()
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_hidden()
 
 
+def test_alertBox(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+
+    # when pop up appears, click on ok.
+    page.on("popup", lambda dialog: dialog.accept())
+    page.get_by_role("button", name="Confirm").click()  # this makes the dialog popup box to appear
+    time.sleep(5)
